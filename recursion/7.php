20540610663d11191a789aad8ef69a788bdb1176
TomoXiang710 <celticwoman710@gmail.com>
2025-01-06 00:06:10 +0900

    Solve problem No.7: Return the first character of the string

---
+++ recursion/7.php
@@ -0,0 +1,12 @@
+/*
+文字列stringInputが与えられるので、
+最初の文字を返す、stringFirst という関数を定義してください。
+
+*/
+
+<?php
+// PHPでは文字型はstring型に含まれる
+function stringFirst(string $stringInput): string{
+    // 関数を完成させてください
+    return $stringInput[0];
+}
