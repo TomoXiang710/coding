b3e48870d8aeca20d28ae8314d8f336028d2f46b
TomoXiang710 <celticwoman710@gmail.com>
2025-01-06 00:10:35 +0900

    Solve problem No.8: Return the last character of the string

---
+++ recursion/8.php
@@ -0,0 +1,12 @@
+/*
+文字列stringInputが与えられるので、
+最後の文字を返す、stringLast という関数を定義してください。
+
+*/
+
+<?php
+
+function stringLast(string $stringInput): string{
+    // 関数を完成させてください
+    return $stringInput[-1];
+}
